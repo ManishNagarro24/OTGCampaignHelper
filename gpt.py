b631282e014d7ddb16a1918670b97d1e89dffb1d
Manish23073143 <2307manish3143@gmail.com>
2023-07-20 17:37:21 +0530

    Manish-4

--- gpt.py
+++ gpt.py
@@ -1,6 +1,6 @@
 import openai
 import json
-openai.api_key = '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'
+openai.api_key = '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'
 def call_gpt(cause,suggestions):
     prompt=f"Your role is to create a fundraiser story for getting donation {cause} cause and these suggestions {suggestions}.Create a fundraising story"
     response = openai.Completion.create(
